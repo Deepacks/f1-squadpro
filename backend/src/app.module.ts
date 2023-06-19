14c65d8e8a91029b10855acce231b100171c2abd
Deepacks <vladimirsette.vc@gmail.com>
2023-06-19 22:35:10 +0200

    fix backend app import case

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 
-import { AuthModule } from './auth/Auth.module'
+import { AuthModule } from './auth/auth.module'
 import { getEnvVar } from './helpers/getEnvVar.helper'
 
 @Module({

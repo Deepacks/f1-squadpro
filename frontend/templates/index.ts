6bd35491d2f6785e2f8b2c91a1b4b0ec17336b85
Deepacks <vladimirsette.vc@gmail.com>
2023-06-20 19:47:22 +0200

    add register ui

--- frontend/templates/index.ts
+++ frontend/templates/index.ts
@@ -1,3 +1,4 @@
 export { DriverPageTemplate } from './driver/DriverPageTemplate'
 export { LoginPageTemplate } from './login/LoginPageTemplate'
+export { RegisterPageTemplate } from './register/RegisterPageTemplate'
 export { TeamsPageTemplate } from './teams/TeamsPageTempalte'
--- frontend/templates/register/components/RegisterForm.tsx
+++ frontend/templates/register/components/RegisterForm.tsx
@@ -9,30 +9,37 @@ import { httpClient } from '@/clients/httpClient'
 
 import { Card, Input, Button } from '@material'
 
-export const LoginForm: FC = memo(() => {
+export const RegisterForm: FC = memo(() => {
   const router = useRouter()
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
 
-  const [isUnauthorized, setIsUnauthorized] = useState(false)
+  const [isAlreadyExisting, setIsAlreadyExisting] = useState(false)
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const handleLoginSubmit = useCallback(
+  const handleRegisterSubmit = useCallback(
     async (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault()
 
-      setIsUnauthorized(false)
+      setIsAlreadyExisting(false)
       setIsSubmitting(true)
 
-      const loginRequest = httpClient.post('/auth/login', { email, password })
+      const registerRequest = httpClient.post('/auth/register', {
+        email,
+        password,
+        firstName,
+        lastName,
+      })
 
       try {
-        await loginRequest
+        await registerRequest
         router.replace('/drivers')
       } catch (error) {
-        if ((error as AxiosError).response?.status === 401) {
-          setIsUnauthorized(true)
+        if ((error as AxiosError).response?.status === 403) {
+          setIsAlreadyExisting(true)
         } else {
           toast.error('Oops, this is on us. Please try again later.')
         }
@@ -40,50 +47,66 @@ export const LoginForm: FC = memo(() => {
         setIsSubmitting(false)
       }
     },
-    [email, password, router],
+    [email, password, firstName, lastName, router],
   )
 
   return (
     <Card className="p-8 w-[400px] max-w-[95%]">
       <h3 className="text-2xl leading-6 text-[color:var(--text-color)] font-bold">
-        Sign In
+        Sign Up
       </h3>
       <p className="mt-1 font-normal text-gray-700">
-        Enter your details to login.
+        Enter your details to create an account.
       </p>
 
       <form
-        onSubmit={handleLoginSubmit}
+        onSubmit={handleRegisterSubmit}
         className="mt-6 w-full max-w-screen-lg"
       >
         <div className="mb-4 flex flex-col gap-6">
           <Input
-            error={isUnauthorized}
             required
             size="lg"
-            label="Email"
-            autoComplete="email"
-            value={email}
-            onChange={({ target: { value } }) => setEmail(value)}
+            label="First Name"
+            autoComplete="given-name"
+            value={firstName}
+            onChange={({ target: { value } }) => setFirstName(value)}
+          />
+          <Input
+            required
+            size="lg"
+            label="Last Name"
+            autoComplete="family-name"
+            value={firstName}
+            onChange={({ target: { value } }) => setLastName(value)}
           />
 
           <div>
             <Input
-              error={isUnauthorized}
+              error={isAlreadyExisting}
               required
-              type="password"
               size="lg"
-              label="Password"
-              autoComplete="current-password"
-              value={password}
-              onChange={({ target: { value } }) => setPassword(value)}
+              label="Email"
+              autoComplete="email"
+              value={email}
+              onChange={({ target: { value } }) => setEmail(value)}
             />
-            {isUnauthorized && (
+            {isAlreadyExisting && (
               <p className="mt-1 text-[color:var(--accent-color)]">
-                Wrong credentials
+                Email already registered
               </p>
             )}
           </div>
+
+          <Input
+            required
+            type="password"
+            size="lg"
+            label="Password"
+            autoComplete="new-password"
+            value={password}
+            onChange={({ target: { value } }) => setPassword(value)}
+          />
         </div>
 
         <Button
@@ -96,12 +119,12 @@ export const LoginForm: FC = memo(() => {
           Register
         </Button>
         <p className="mt-4 text-center font-normal text-gray-700">
-          You don't have an account?{' '}
+          You already have an account?{' '}
           <Link
-            href="/register"
+            href="/login"
             className="font-medium text-blue-500 transition-colors hover:text-blue-700"
           >
-            Sign Up
+            Sign In
           </Link>
         </p>
       </form>
